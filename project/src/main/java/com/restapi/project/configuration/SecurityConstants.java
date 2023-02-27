bbd68f23094b8c6348dbfa35bc1fae0d4a6f30ed
czarus98 <czarek.wojcik98@gmail.com>
2023-02-27 19:49:14 +0100

    Removed confidential file

--- project/src/main/java/com/restapi/project/configuration/SecurityConstants.java
+++
@@ -1,8 +0,0 @@
-package com.restapi.project.configuration;
-
-public class SecurityConstants {
-    public static final String SIGNING_KEY = "SecretKey";
-    public static final long EXPIRATION_TIME = 864_000_000; // 10 days
-    public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String HEADER_STRING = "auth";
-}
